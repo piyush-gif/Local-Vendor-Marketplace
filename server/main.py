07d09879fc22e92a3d5c310dc0738fbd02ac0621
piyush <piyush.khadka2233@gmail.com>
2026-09-18 12:37:07 +0545

    CORS access

--- server/main.py
+++ server/main.py
@@ -9,7 +9,10 @@ app = FastAPI(title="Vendor Marketplace API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://local-vendor-marketplace.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
